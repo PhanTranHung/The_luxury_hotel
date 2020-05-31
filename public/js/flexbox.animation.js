b04e7d22b1034ff495208a19b2645b39696feff2
phantranhung <17it013@sict.udn.vn>
2020-06-01 00:11:33 +0700

    Phan Tran Hung push 2

--- public/js/flexbox.animation.js
+++ public/js/flexbox.animation.js
@@ -3,20 +3,24 @@
 
     function onClick(e) {
         var $target = $(e.target);
-        if ($target.hasClass('slide') && !$target.hasClass('active') && !$target.siblings().hasClass('active')) {
-            $target.removeClass('anim-in last-viewed').addClass('active')
+        let container = $target.parent();
+        if ($target.hasClass('flexbox') && !$target.hasClass('active') && !$target.siblings().hasClass('active')) {
+            $target.removeClass('anim-in last-viewed').addClass('active');
+            container.removeClass('room-out').addClass('zoom-in');
             $target.siblings().removeClass('anim-in last-viewed').addClass('anim-out');
         }
     }
 
     function closeSlide(e) {
         var $slide = $(e.target).parent();
+        let container = $slide.parent();
         $slide.removeClass('active anim-in').addClass('last-viewed');
+        container.removeClass('zoom-in').addClass('zoom-out');
         $slide.siblings().removeClass('anim-out').addClass('anim-in');
     }
 
     $(function() {
-        $slides = $('.slide');
+        $slides = $('.flexbox');
         $btnArr = $slides.find('.btn-close');
         $slides.on('click', onClick);
         $btnArr.on('click', closeSlide);
